fadb3bf3e4b2a2b376ebf6a68a0088708986c211
Shammah Ranks <shammahodhiambo@gmail.com>
2021-10-06 08:28:52 +0300

    Cart section addition

---
+++ src/components/ShopPage/Cart.jsx
@@ -0,0 +1,55 @@
+import ProcessPayment from "./ProcessPayment.jsx";
+import { products } from "../database";
+import { useState, useEffect } from "react";
+
+function Cart() {
+  const [totalCost, setTotalCost] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
+  useEffect(() => {
+    const prices = products.map((product) => {
+      return product.price;
+    });
+    const total = prices.reduce((a, b) => a + b);
+
+    setTotalCost(totalCost + total);
+    setItemCount(products.length);
+
+    return () => {
+      console.log("Calculation Done", total);
+    };
+  }, []);
+
+  return (
+    <section className="checkout-section">
+      <div className="cart-items">
+        <span className="cart-intro">
+          <h1>Shopping Cart</h1>
+          <h5>Price</h5>
+        </span>
+        {products.map((product) => {
+          return (
+            <div className="cart-item" key={product.id}>
+              <img src={product.img} alt="Cart Item" />
+              <div className="cart-item-details">
+                <h4>{product.name}</h4>
+                <p>{product.details}</p>
+                <h5>by The Mics Are Open</h5>
+                <strong>In Stock</strong>
+                <select name="quantity" id="">
+                  <option value="1">Qty:</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+              </div>
+              <span className="item-price">Ksh. {product.price}</span>
+            </div>
+          );
+        })}
+      </div>
+      <ProcessPayment totalCost={totalCost} itemCount={itemCount} />
+    </section>
+  );
+}
+
+export default Cart;
