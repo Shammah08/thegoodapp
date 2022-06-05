dc0b77c9991d0cbc888ac13fa8a1ec0da523d49f
steveshammah <shammahodhiambo@gmail.com>
2022-06-05 21:01:38 +0300

    fix: slider animation fix

--- src/components/LandingPage/Slide.jsx
+++ src/components/LandingPage/Slide.jsx
@@ -1,32 +1,30 @@
 import { Link } from "react-router-dom";
 
-const Slide = ({ slide }) => {
+const Slide = ({ slide, active }) => {
+
   return (
-    <div className="slide-container" key={Math.floor() * 1000}>
-      <div className="container">
-        <div className="text-container">
-          
-            {slide.title.split("-").map((text, index) => {
-              return (
-                
-                  <h2 key={'title' + index}>{text}</h2>
-                
-              );
-            })}
-          
+      <div className="slide-container" key={Math.floor() * 1000}>
+      <div className="container" style={{ display: active ? 'block' : 'none' }}>
+              <div className="text-container">
+                  {slide.title.split('-').map((text, index) => {
+                      return <h2 key={'title' + index}>{text}</h2>
+                  })}
 
-          <p>{slide.desc}</p>
-          <Link to={slide.url}>
-            <button className="btn">{slide.button}</button>
-          </Link>
-        </div>
+                  <p>{slide.desc}</p>
+                  <Link to={slide.url}>
+                      <button className="btn">{slide.button}</button>
+                  </Link>
+              </div>
 
-        <div className="image-container">
-          <img src={slide.image} alt="" />
-        </div>
+              <div className="image-container">
+                  <img src={slide.image} alt="" />
+              </div>
+          </div>
       </div>
-    </div>
-  );
+  )
 };
 
 export default Slide;
+
+
+
